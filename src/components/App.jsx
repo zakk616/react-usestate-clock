e7664d886f558afbe8482bb48e447b0072f47916
zakk <muhammadzakaria616@gmail.com>
2022-11-06 19:25:13 +0500

    added time formate

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,23 @@
 import React, { useState } from "react";
 
 function App() {
-  let t = new Date().toLocaleTimeString();
-  const [time, setTime] = useState(t);
+  const [time, setTime] = useState();
 
   function getTime() {
-    setTime(new Date().toLocaleTimeString());
+    setTime(formatAMPM(new Date()));
+  }
+
+  function formatAMPM(date) {
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var seconds = date.getSeconds();
+    var ampm = hours >= 12 ? "PM" : "AM";
+    hours = hours % 12;
+    hours = hours ? hours : 12; // the hour '0' should be '12'
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+    var strTime = hours + ":" + minutes + ":" + seconds + " " + ampm;
+    return strTime;
   }
 
   setInterval(getTime, 1000);
